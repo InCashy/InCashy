0da2161690f663dbee6162f1e50d50ddc2f4c761
Angel Dev <75772159+Angel-999@users.noreply.github.com>
2025-07-01 11:28:26 -0700

    incashy changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,27 +3,17 @@ import { CommonModule, DatePipe } from '@angular/common';
 import { Client } from 'appwrite';
 import { environment } from '../environments/environment';
 import { RouterOutlet } from '@angular/router';
+import { LoginComponent } from './login/login.component';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   standalone: true,
   styleUrls: ['./app.component.css'],
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, LoginComponent],
 })
 export class AppComponent{
 
-  // Environment variables - you'll need to set these up in your environment.ts
-  endpoint = environment.appwriteEndpoint;
-  projectId = environment.appwriteProjectId;
-  projectName = environment.appwriteProjectName;
-
-  private client: Client;
-
-  constructor(private zone: NgZone) {
-    this.client = new Client()
-      .setEndpoint(environment.appwriteEndpoint)
-      .setProject(environment.appwriteProjectId);
-  }
+  isLoggedIn = false;
 
 }
